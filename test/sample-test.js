df7a9457da5c28c773183f4d04361a7b86d19cee
Michael FIG <michael+github@fig.org>
2019-08-15 17:37:59 -0600

    test-sample.js: use test.only as the default

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,6 +1,7 @@
 import { test } from 'tape-promise/tape';
 
-test('unique test description', async t => {
+// TODO: remove .only when you have this test working
+test.only('unique test description', async t => {
   try {
     t.true('various tests');
     t.equal(await Promise.resolve(123), 123);
